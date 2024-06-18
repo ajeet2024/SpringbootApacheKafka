937bd0f3f88ea568e7bd60438c8e6b3e7b9e3826
ajeet2024 <ajeet.ajeet2024@gmail.com>
2024-06-18 19:36:40 +0530

    first commit

---
+++ src/main/java/com/ajeet/SpringbootApacheKafka.java
@@ -0,0 +1,14 @@
+package com.ajeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApacheKafka {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootApacheKafka.class, args);
+	}
+
+}
